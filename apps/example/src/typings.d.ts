0ee49f3ceaaa26efc56c05cf46560c871ec37854
Andres Guerrero <andresmgsl@gmail.com>
2022-09-14 18:58:53 +0100

    chore: added misssing type

--- apps/example/src/typings.d.ts
+++ apps/example/src/typings.d.ts
@@ -2,9 +2,7 @@ declare module 'cytoscape-node-html-label' {
     declare global {
     namespace cytoscape {
         interface Core {
-            /*
-             * Set up the context menu according to the given options.
-             */
+
             nodeHtmlLabel: (...options?: any) => any;
         }
     }
